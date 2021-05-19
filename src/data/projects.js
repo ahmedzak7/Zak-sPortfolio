f83dfb70ea0e2fbc4dc2341cd134e1bcec89519e
zak <ahmedzak19972008@gmail.com>
2021-05-19 00:04:25 -0400

    1st project

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,11 +2,11 @@ import {v4 as uuidv4 } from "uuid"
 export const projects = [
     {
         id: uuidv4(),
-        title: "Project One",
-        desc: "Lorem ipsum doler sit amet consecteture dfdfgdsf elit",
+        title: "Deploy PHP website on AWS EC2 using Apache",
+        desc: "1-select a region 2-Create A vpc and a subnet within 3-Create security groups 4-Launch EC2 5-Configure Inbound &Outbound rules 6- create Internet gateway to make ec2 respond 7-Configure aws by installing apache with php",
         github: "https://github.com/ahmedzak7",
         demo: "https://github.com/ahmedzak7",
-        image: "./images/project-1.jpg",
+        image: "./images/1stproject.png",
     },
     {
         id: uuidv4(),
