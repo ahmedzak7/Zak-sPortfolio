f477bf7978725baf2724a76d394d2bdff09ba93e
zak <ahmedzak19972008@gmail.com>
2021-05-24 11:49:54 -0400

    Updated Projects

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -10,8 +10,8 @@ const Homepage = () => {
             DevOps Engineer,Full Stack Developer
         </p>
         <ul>
-            <li><a href="https://github.com/ahmedzak7/Zak-sPortfolio" target="_blank" rel="noopenner noreferrer"><FaLinkedin /></a></li>
-            <li><a href="https://github.com/ahmedzak7/Zak-sPortfolio" target="_blank" rel="noopenner noreferrer"><FaGithub /></a></li>
+            <li><a href="https://www.linkedin.com/in/ahmed-zakaria-20184a146/" target="_blank" rel="noopenner noreferrer"><FaLinkedin /></a></li>
+            <li><a href="https://github.com/ahmedzak7" target="_blank" rel="noopenner noreferrer"><FaGithub /></a></li>
         </ul>
         </div>
         </section>
